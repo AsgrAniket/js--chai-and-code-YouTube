7b946f3d81b7ad2930bc930f2218fff8f9fdb075
Aniket Ribadiya <159411333+AsgrAniket@users.noreply.github.com>
2024-11-03 03:43:50 +0000

    String properties and experimentation

--- 01_Basics/string.js
+++ 01_Basics/string.js
@@ -3,5 +3,27 @@ const repoCount = 5
 
 // console.log(name + repoCount + "value"); Old fashioned
 
-console.log(`My name is ${name} and my repoCount is ${repoCount}`);
+// console.log(`My name is ${name} and my repoCount is ${repoCount}`);
 
+const gameName = new String ('aniketfc')
+
+// console.log(gameName [0]);
+
+// console.log (gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.charAt (4));
+// console.log(gameName.indexOf ('e'));
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLocaleUpperCase());
+
+let myName = new String ("    Aniket   ")
+// console.log(myName.trimStart());
+// console.log(myName.trimEnd());
+// console.log(myName.trim());
+myName = "Aniket%20Asgr    "
+// console.log(myName.replace("%20", " "));
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 7);
+console.log(anotherString);
