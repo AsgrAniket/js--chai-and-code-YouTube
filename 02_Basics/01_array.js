8d8268d9a539258413ad6d779fc460342d4e2ced
Aniket Ribadiya <159411333+AsgrAniket@users.noreply.github.com>
2024-12-29 06:54:33 +0000

    Add file to version control

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,10 @@
+// array 
+
+const myArr = [0, 1, 2, 3, 3, 4]
+const myHeroes = ["MonkeyDLuffy", "Father"]
+
+console.log(myArr[2]);
+console.log(myHeroes[1]);
+
+const myArr2 = new Array [0, 1, 2, 4, 3]
+console.log(myArr[1]);
