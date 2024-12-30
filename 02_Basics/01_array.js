ceba5af1b0845bad02146a9ba9af68ba0f5e6993
Aniket Ribadiya <159411333+AsgrAniket@users.noreply.github.com>
2024-12-30 04:56:10 +0000

    Track all file

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -1,10 +1,24 @@
 // array 
 
-const myArr = [0, 1, 2, 3, 3, 4]
+const myArr = [0, 1, 2, 3, 4, 5]
 const myHeroes = ["MonkeyDLuffy", "Father"]
 
 console.log(myArr[2]);
 console.log(myHeroes[1]);
 
-const myArr2 = new Array [0, 1, 2, 4, 3]
+const myArr2 = new Array (0, 1, 2, 4, 3)
 console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(10)    // adds value 10 at last index of array 
+// myArr.push(7)    // adds value 7 at last index of array 
+// myArr.pop()      // last digit will be removed 
+// console.log(myArr);
+
+// myArr.unshift(8)     // adds value to the starting index
+// myArr.shift()       // removes value to the starting index
+// console.log(myArr);  // Here removes 0 from 0th index
+
+console.log(myArr.includes(5));   // checks existence of a element (values)
+console.log(myArr.indexOf(4));   // checks the value of a index 
