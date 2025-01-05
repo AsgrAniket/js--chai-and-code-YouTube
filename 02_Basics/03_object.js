b4a4c691bf5a42544fed3d1c8b623f6ba055df7f
Aniket Ribadiya <159411333+AsgrAniket@users.noreply.github.com>
2025-01-05 05:58:38 +0000

    Track all files

--- 02_Basics/03_object.js
+++ 02_Basics/03_object.js
@@ -11,13 +11,30 @@ const JsUser = {
     location: "Ahmedabad", 
     [mySym]: "mykey1",
     email: "aniketribadiya@google.com",
+    isLoggedIn: true,
     lastloginDay: ["Monady", "Saturday"]
 }
 
 // myArray = ["A", "n"] // in arrays there is predefined key starting form 0 to n
 
-console.log(JsUser.email);
-console.log(JsUser["age"]);
-console.log(JsUser["name"]);
-console.log(typeof JsUser.mySym);
-console.log(JsUser[mySym]);
+// console.log(JsUser.email);
+// console.log(JsUser["age"]);
+// console.log(JsUser["name"]);
+// console.log(JsUser.mySym);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "aniket123@gmail.com"
+Object.freeze(JsUser)   // no changes or updates can be done after implementation of freeze
+JsUser.email = "aniket123chatgpt.com"
+console.log(JsUser["email"]);
+
+JsUser.greeting = function() {
+    console.log("Hello JS user");
+}
+    JsUser.greeting()
+
+JsUser.greetingtwo = function() {
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greetingtwo());
